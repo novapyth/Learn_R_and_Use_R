5de80babe8ee09d0eef2dda557ca75532598f0f6
Alisha Parveen <49845542+aparveen-prog@users.noreply.github.com>
2020-06-21 14:45:19 +0200

    Create DAY4.R

---
+++ DAY4.R
@@ -0,0 +1,16 @@
+c) counting a characters in a stringi
+nchar(x)
+result <- nchar("Count the number of characters")
+print(result)
+
+d) Changing the case - toupper() & tolower() functions
+# Changing to Uppercase.
+result <- toupper("Changing To Upper")
+print(result)
+# Changing to lower case.
+result <- tolower("Changing To Lower")
+print(result)
+e) Extracting parts of a string - substring() function substring(x,first,last)
+# Extract characters from 5th to 7th position.
+result <- substring("Extract", 5, 7)
+print(result)
