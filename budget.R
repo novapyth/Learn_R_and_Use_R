e0807cf154203e044bd5924f657a8bc594e80e2b
drabhishekkumar <draabhishekkumar@gmail.com>
2020-06-30 22:19:50 +0530

    create budget.R
    one example

---
+++ budget.R
@@ -0,0 +1,42 @@
+#mymonthlybudget
+#income
+i=5000
+#Print i
+#i
+#eating
+e=3000
+#Print e
+e
+
+#cinema
+c=1000
+#Print c
+c
+#donation
+d=500
+#Print d
+d
+#saving
+s=5000-3000-500-1000
+#Print s
+s
+#Annual saving
+AnS=s*12
+#Print AnS
+AnS
+#Annual saving for 40 years
+fourtyYRS=AnS*40
+fourtyYRS
+#BLR population size 15000
+BLRpop<- 15000
+#Print BLRpop
+BLRpop
+#Annual saving of population size 15000
+AsBLR<-BLRpop*s
+#Print AsBLR
+AsBLR
+#Total saving for 40 years of population size 15000
+BLRas=BLRpop*fourtyYRS
+#Print BLRas
+BLRas
+#Defining variables in R by two ways
